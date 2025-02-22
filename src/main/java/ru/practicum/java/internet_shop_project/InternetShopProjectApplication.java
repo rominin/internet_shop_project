275c0321a2e2752805ff13ecec0fb0f7074ca1e7
Roman Orinichev <roma-orinich@yandex.ru>
2025-02-22 12:41:45 +0100

    init commit

---
+++ src/main/java/ru/practicum/java/internet_shop_project/InternetShopProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.java.internet_shop_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetShopProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetShopProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/practicum/java/internet_shop_project/InternetShopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.practicum.java.internet_shop_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
