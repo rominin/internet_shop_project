d36e3334c1e8eb98b1abf9f40065f5ef60b81fbe
Roman Orinichev <roma-orinich@yandex.ru>
2025-02-23 23:46:06 +0100

    implemented OrderService

---
+++ src/main/java/ru/practicum/java/internet_shop_project/service/OrderService.java
@@ -0,0 +1,65 @@
+package ru.practicum.java.internet_shop_project.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.java.internet_shop_project.entity.Cart;
+import ru.practicum.java.internet_shop_project.entity.Order;
+import ru.practicum.java.internet_shop_project.entity.OrderItem;
+import ru.practicum.java.internet_shop_project.repository.CartItemRepository;
+import ru.practicum.java.internet_shop_project.repository.CartRepository;
+import ru.practicum.java.internet_shop_project.repository.OrderItemRepository;
+import ru.practicum.java.internet_shop_project.repository.OrderRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Order getOrderById(Long orderId) {
+        return orderRepository.findById(orderId)
+                .orElseThrow(() -> new RuntimeException("Order not found"));
+    }
+
+    @Transactional
+    public Order createOrderFromCart() {
+        Cart cart = cartRepository.findById(1L)
+                .orElseThrow(() -> new RuntimeException("Cart not found"));
+
+        if (cart.getCartItems().isEmpty()) {
+            throw new RuntimeException("Cart is empty");
+        }
+
+        Order order = Order.builder()
+                .totalPrice(cart.getTotalPrice())
+                .build();
+
+        orderRepository.save(order);
+
+        List<OrderItem> orderItems = cart.getCartItems().stream()
+                .map(cartItem -> new OrderItem(
+                        null,
+                        order,
+                        cartItem.getProduct(),
+                        cartItem.getQuantity()
+                ))
+                .toList();
+
+        orderItemRepository.saveAll(orderItems);
+
+        cartItemRepository.clearCartItems(cart.getId());
+
+        return order;
+    }
+
+}
