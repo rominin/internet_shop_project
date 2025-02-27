db4004c215eeca7275155d66a3d9114dd9b72881
Roman Orinichev <roma-orinich@yandex.ru>
2025-02-27 23:38:49 +0100

    added pre-generated test data

--- src/main/java/ru/practicum/java/internet_shop_project/entity/Product.java
+++ src/main/java/ru/practicum/java/internet_shop_project/entity/Product.java
@@ -20,7 +20,7 @@ public class Product {
     @Column(nullable = false, unique = true)
     private String name;
 
-    @Column
+    @Column(columnDefinition = "TEXT")
     private String imageUrl;
 
     @Column(length = 500)
